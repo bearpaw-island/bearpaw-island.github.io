7f0b92d52ebb80e948014b60c13974f9ad663d15
bearpaw-island <3139287629@qq.com>
2026-07-06 18:42:45 +0800

    fix: JS注入bscene-body宽度修复

--- scene.js
+++ scene.js
@@ -40,6 +40,7 @@ function openScene(bid) {
       (isMax ? '<button class="ubtn max" disabled>已满级</button>' : '<button class="ubtn" id="scn-upgrade">升级 ' + fmt(cost(bid)) + '</button>') +
     '</div></div>';
   panel.style.display = 'block';
+  setTimeout(function(){ var bd=document.getElementById('scn-body'); if(bd){ bd.style.width='100%'; bd.style.display='block'; } }, 50);
 
   renderScene(bid, lv);
   document.getElementById('scn-back').onclick = closeScene;
